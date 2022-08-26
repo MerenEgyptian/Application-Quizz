4a0dd7d149c7b48548813f7dca4ca527f60caf71
Monsieur Doudou <103032864+MerenEgyptian@users.noreply.github.com>
2022-08-26 10:35:22 +0200

    Affichage des questions et des réponses
    Il manque les quatrièmes réponses ( 'À se connecter à une base de données' et 'Aucune réponse n'est vrai' )
    pour les questions ( 'À quoi sert HTML' et 'Qu'est ce qu'un navigateur web' ? )

--- quizz.js
+++ quizz.js
@@ -14,40 +14,10 @@ promiseExamen
   .then(function (valeurs) {
     console.log(valeurs);
 
-    const examen = valeurs["question"];
-    //console.log("examen.length", examen.length);
+    const examens = valeurs["question"];
+    //console.log("examens.length", examens.length);
 
-    for (let i = 0; i < examen.length; i++) {
-      const element = examen[i];
-      const id = element.id; //console.log("id", id)
-      const card = document.createElement("div")
-      card.classList.add("card", "my-2")
 
-      const title = document.createElement("h5")
-      title.textContent = element.question
-      title.classList.add("card-title")
-      //console.log(title)
-
-      for (let j = 0, a = 1; j < examen.length; j++, a++) {
-        const cardHeader = document.createElement("div")
-        cardHeader.classList.add("card-header")
-        console.log("a", a)
-        const eltOptionsption = element.options[a].option
-        cardHeader.textContent = eltOptionsption
-        console.log(cardHeader)
-      }
-
-      const cardBody = document.createElement("div")
-      cardBody.classList.add("card-body")
-
-      document
-        .getElementById("app")
-        .append(title, cardHeader, card, cardBody);
-    }
-
-    //var elt = document.querySelector('h1')
-    //console.log("elt", elt.classList);
-    //console.log(elt.classList.contains('masthead-heading'));
     const titreTitle = valeurs.title;
     titreTitle.textContent = titreTitle;
     //console.log(elt, titreTitle);
@@ -55,7 +25,38 @@ promiseExamen
       .querySelector('h1')
       .append(titreTitle)
 
+
+    for (let i = 0; i < examens.length; i++) {
+
+      const element = examens[i];
+
+      const title = document.createElement("h5")
+      title.textContent = element.question
+      title.classList.add("card-title")
+      //console.log(title)
+
+      const cardBody = document.createElement("div")
+      cardBody.classList.add("card-body")
+
+      document
+        .getElementById("app")
+        .append(title, cardBody);
+
+
+      for (let j = 1; j < examens.length; j++) {
+        const cardHeader = document.createElement("div")
+        cardHeader.classList.add("card-header")
+        cardHeader.textContent = element.options[j].option
+        console.log(cardHeader)
+
+        document
+          .getElementById("app")
+          .append(cardHeader);
+
+      }
+    }
   })
+
   .catch(function (err) {
     // Une erreur est survenue
   });
