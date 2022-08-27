d1c7682e7212386a2960bfa61fac28a3a6656e65
Monsieur Doudou <103032864+MerenEgyptian@users.noreply.github.com>
2022-08-27 17:30:45 +0200

    Affichage de la liste à puce
    Manque toujours les deux éléments réponses du questionnaire HTML
    Affichage identique à la page Examens

--- quizz.js
+++ quizz.js
@@ -12,18 +12,15 @@ promiseExamen
   })
 
   .then(function (valeurs) {
-    console.log(valeurs);
 
     const examens = valeurs["question"];
-    //console.log("examens.length", examens.length);
 
+    const titreExamen = valeurs.title;
+    titreExamen.textContent = titreExamen;
 
-    const titreTitle = valeurs.title;
-    titreTitle.textContent = titreTitle;
-    //console.log(elt, titreTitle);
     document
       .querySelector('h1')
-      .append(titreTitle)
+      .append(titreExamen)
 
 
     for (let i = 0; i < examens.length; i++) {
@@ -33,25 +30,35 @@ promiseExamen
       const title = document.createElement("h5")
       title.textContent = element.question
       title.classList.add("card-title")
-      //console.log(title)
 
-      const cardBody = document.createElement("div")
-      cardBody.classList.add("card-body")
+      const cardHeader = document.createElement("div")
+      cardHeader.classList.add("card-header")
+
+      var ul = document.createElement('ul')
+
 
       document
         .getElementById("app")
-        .append(title, cardBody);
+        .append(title, cardHeader, ul);
 
 
       for (let j = 1; j < examens.length; j++) {
-        const cardHeader = document.createElement("div")
-        cardHeader.classList.add("card-header")
-        cardHeader.textContent = element.options[j].option
-        console.log(cardHeader)
+        const cardBody = document.createElement("div")
+        cardBody.classList.add("card-body")
+        var li = document.createElement('li')
+        ul.appendChild(li)
+        cardBody.textContent = element.options[j].option
+        li.innerText = li.innerText + cardBody.textContent
+        console.log(cardBody)
 
-        document
-          .getElementById("app")
-          .append(cardHeader);
+        document.getElementsByClassName("container")[i]
+          .style.cssText = "width: 70%; margin-left: 12.5em"
+
+        document.getElementsByClassName("card-header")[i]
+          .style.marginBottom = "1em"
+
+        document.getElementById("app")
+          .style.cssText = "width: 45%; margin-left: 15em; color: teal"
 
       }
     }
